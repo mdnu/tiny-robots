fb2c4ebfdf320b87fcf7b9ccc1cbb4f8a0156000
mdnu <mark@sdentertainment.ca>
2016-01-07 05:00:51 -0500

    Edit

--- CoffeeRec/app/src/main/java/numd/coffeerec/CoffeeRec.java
+++ CoffeeRec/app/src/main/java/numd/coffeerec/CoffeeRec.java
@@ -6,15 +6,15 @@ import java.util.List;
 public class CoffeeRec {
     List<String> getRec(String flavor) {
         List<String> types = new ArrayList<String>();
-        if (flavor.equals("Acidic - Washed Arabica(Above 4,000ft)")) {
+        if (flavor.equals("Acidic - Washed Arabica (Above 4,000ft)")) {
             types.add("Acidy: Colombian Excelso");
             types.add("Nippy: Costa Rica SHB");
             types.add("Piquant: Kenya AA");
-        } else if (flavor.equals("Mellow - Washed Arabica(Below 4,000ft)")) {
+        } else if (flavor.equals("Mellow - Washed Arabica (Below 4,000ft)")) {
             types.add("Melow: Sumatra - Indonesia");
             types.add("Mild: Guatemala");
             types.add("Delicate: Papua New Guinea");
-        } else if (flavor.equals("Winey - Unwashed Arabica(Above 4,000ft)")) {
+        } else if (flavor.equals("Winey - Unwashed Arabica (Above 4,000ft)")) {
             types.add("Winey: Ethiopia - Djimma");
             types.add("Tart: Kivu - Congo");
             types.add("Tangy: India Arabica");
@@ -27,7 +27,7 @@ public class CoffeeRec {
             types.add("Sharp: Ivory Coast");
             types.add("Astrigent: Indonesian");
             types.add("Rough: Angola");
-        } else if (flavor.equals("Soury - Unwashed Brazils (Above 2,000')")) {
+        } else if (flavor.equals("Soury - Unwashed Brazils (Above 2,000ft)")) {
             types.add("Soury: Vitoria - Brazil");
             types.add("Hard: Minas - Brazil");
             types.add("Acrid: Rio - Brazil");
